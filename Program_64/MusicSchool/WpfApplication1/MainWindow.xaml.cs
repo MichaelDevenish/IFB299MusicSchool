3f602f434f292c3fff370b120a8c83e32df97e8c
Michael Devenish <michael.devenish@connect.qut.edu.au>
2016-09-07 21:58:20 +1000

    setup databindings for timetables

--- Program_64/MusicSchool/WpfApplication1/MainWindow.xaml.cs
+++ Program_64/MusicSchool/WpfApplication1/MainWindow.xaml.cs
@@ -20,13 +20,22 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
         public bool logged_in = false;
+        public List<HalfHour> allTimetables;
+        public List<HalfHour> myTimetables;
 
         public MainWindow()
         {
-            
+
             InitializeComponent();
+            allTimetables = GenerateTimetableDataBindings();
+            myTimetables = GenerateTimetableDataBindings();
+            allClassesTable.ItemsSource = allTimetables;
+            myClassesTable.ItemsSource = myTimetables;
+
+            myTimetables[1].Monday = "hello";
+            //add database information to tables 
         }
 
         //Each time the user changes the tab, check if the login status has changes and make the appropriate
@@ -35,15 +44,62 @@ namespace WpfApplication1
         {
             if (logged_in)
             {
-                errorMessage.Visibility = System.Windows.Visibility.Hidden;
+                //errorMessage.Visibility = System.Windows.Visibility.Hidden;
                 //timetable_ret.Visibility = System.Windows.Visibility.Visible;
             }
             else
             {
-                errorMessage.Visibility = System.Windows.Visibility.Visible;
+                //errorMessage.Visibility = System.Windows.Visibility.Visible;
                 //timetable_ret.Visibility = System.Windows.Visibility.Hidden;
 
             }
         }
+
+        private List<HalfHour> GenerateTimetableDataBindings()
+        {
+            //do database code to get the all timetable slots
+
+            List<HalfHour> Days = new List<HalfHour>();
+            for (int i = 0; i <= 16; i++)
+            {
+                string time = "";
+                bool halfhour = false;
+                bool am = true;
+                if (i % 2 == 1) halfhour = true;
+                if (i >= 6) am = false;
+                int hour = (((i + 18) % 24) / 2);
+                if (hour == 0) hour = 12;
+
+                if (halfhour && am) time = hour + ":30am";
+                else if (halfhour && !am) time = hour + ":30pm";
+                else if (!halfhour && am) time = hour + ":00am";
+                else if (!halfhour && !am) time = hour + ":00pm";
+
+                Days.Add(new HalfHour()
+                {
+                    Time = time,
+                    Monday = "",
+                    Tuesday = "",
+                    Wednesday = "",
+                    Thursday = "",
+                    Friday = "",
+                    Saturday = "",
+                    Sunday = ""
+                });
+            }
+            return Days;
+        }
+    }
+
+    public class HalfHour
+    {
+        public string Time { get; set; }
+        public string Sunday { get; set; }
+        public string Monday { get; set; }
+        public string Tuesday { get; set; }
+        public string Wednesday { get; set; }
+        public string Thursday { get; set; }
+        public string Friday { get; set; }
+        public string Saturday { get; set; }
     }
 }
