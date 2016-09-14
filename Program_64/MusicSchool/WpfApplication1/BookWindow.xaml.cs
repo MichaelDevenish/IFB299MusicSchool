ff736963fa5761f9cbf3120ea13487eb5250fd0a
Patrick <patrickbd91@yahoo.com.au>
2016-09-14 23:41:49 +1000

    booking window

--- Program_64/MusicSchool/WpfApplication1/BookWindow.xaml.cs
+++ Program_64/MusicSchool/WpfApplication1/BookWindow.xaml.cs
@@ -29,8 +29,11 @@ namespace WpfApplication1
             this.parentWindow = parentWindow;
             this.studentID = studentID;
 
-            //selectDate.SelectedDate = DateTime.Now.;
+            selectDate.SelectedDate = DateTime.Now.Date;
 
+
+
+            //Get the names of the teachers and populate the teacher combobox
             teacherinfo = db.ReadTeacherInfo();
             List<string> teacherNames = new List<string>();
             foreach(string[] name in teacherinfo)
@@ -38,6 +41,24 @@ namespace WpfApplication1
                 teacherNames.Add(name[1] + " " + name[2]);
             }
             selectTeacher.ItemsSource = teacherNames;
+
+            string query = "SELECT lesson_date, lesson_length FROM lessons";
+            List<string[]> lessons = db.simpleConnection(false, query, null);
+
+            int[,] bookArray = new int[90, 13];
+
+            foreach (string[] day in lessons)
+            {
+                DateTime currentDate = Convert.ToDateTime(day[0]);
+
+                if (DateTime.Now.CompareTo(currentDate) < 0 && (currentDate.Hour - 9 )* 2 < 13 && (currentDate.Hour - 9) * 2 >= 0)
+                {
+                    System.TimeSpan diff = currentDate - DateTime.Now;
+                    bookArray[diff.Days, (currentDate.Hour - 9) *2] = 1;
+                    if(day[1] == "1") bookArray[diff.Days, (currentDate.Hour - 9) * 2 + 1] = 1;
+                }
+
+            }
             string[] validTimes = new string[13] { "9:00am", "9:30am", "10:00am",
                                                      "10:30am", "11:00am", "11:30am", "12:00pm", "12:30pm",
                                                       "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm"};
